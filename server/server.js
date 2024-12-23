4fbd3765aa6394f7ba2538e9741d832cdc8a34c3
Alex Tomack <alexander.e.tomack@vanderbilt.edu>
2024-12-23 11:12:12 -0500

    added cors
    adjusted redirect URI in spotify admin

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const spotifyWebApi = require('spotify-web-api-node');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
         redirectUri: 'http://localhost:3000',
         clientId: 'c4b183eb179c42208b6e70e4ba0187bb',
-        clientSecret:''
+        clientSecret:'9a5168342c724eb6ad9732b9e7b4feb4'
     })
 
     spotifyApi.authorizeCodeGrant(code).then(data => {
@@ -21,3 +23,5 @@ app.post('/login', (req, res) => {
         res.sendStatus(400)
     })
 })
+
+app.listen(3001);
