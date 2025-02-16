3efbd4015c460db4c2a48d2452612828014aca67
Alex Tomack <alexander.e.tomack@vanderbilt.edu>
2025-02-16 17:46:54 -0600

    useAuth refresh token timeout adjusted.

--- client/src/app/useAuth.js
+++ client/src/app/useAuth.js
@@ -1,18 +1,51 @@
 "use client";
 
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function useAuth(code) {
-    const [accessToken, setAccessToken] = useState()
-    const [refreshToken, setRefreshToken] = useState()
-    const [expiresIn, setExpiresIn] = useState()
+    const [accessToken, setAccessToken] = useState();
+    const [refreshToken, setRefreshToken] = useState();
+    const [expiresIn, setExpiresIn] = useState();
+    const [error, setError] = useState(null);
+
+
+    console.log(refreshToken)
 
     useEffect(() => {
-        axios.post('http:localhost:3001', {
-            code,
-        }).then(res => {
-            console.log(res.data)
-        })
+        if (!code) return;
+        axios
+            .post('http://localhost:3001/refresh', { refreshToken })
+            .then((res) => {
+                setAccessToken(res.data.accessToken);
+                setRefreshToken(res.data.refreshToken);
+                setExpiresIn(res.data.expiresIn);
+                window.history.pushState({}, null, "/");
+            })
+            .catch((err) => {
+                console.error("Authentication failed:", err);
+                setError("Authentication failed. Please try again.");
+                window.location = "/";
+            })
     }, [code])
+    
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return;
+        const interval = setInterval(() => {
+            axios
+            .post("http://localhost:3001/refresh", { 
+                refreshToken,
+            })
+            .then(res=> {
+                setAccessToken(res.data.accessToken);
+                setExpiresIn(res.data.expiresIn);
+            })
+            .catch(()=> {
+                window.location = "/"
+            })
+        }, (expiresIn-60) *1000)
+        return () => clearInterval(interval)
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
